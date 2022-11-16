34b85c80f710edfc903ff49d3c374ff92f1db6cd
Александр Пьячев <urbanmen132@yandex.ru>
2022-11-16 09:22:48 +0900

    Templates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
 def group_posts(request, slug):
-    return HttpResponse(f"Групповые посты {slug}")
+    template = 'posts/group_list.html'
+    title = 'Группы'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'content': 'Здесь будет информация о группах проекта Yatube',
+    }
+    return render(request, template, context)
 
 
 def index(request):
-    return HttpResponse("Главная страница")
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # Словарь с данными принято называть context
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
