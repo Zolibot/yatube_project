e4df47115807100874e944af1ed101459867734f
Александр Пьячев <urbanmen132@yandex.ru>
2022-11-17 18:11:41 +0900

    Changed flake8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,15 +3,7 @@ from .models import Post, Group
 
 
 def group_posts(request, slug):
-    # Функция get_object_or_404 получает по заданным критериям объект 
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
     group = get_object_or_404(Group, slug=slug)
-
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления
-    # условия WHERE group_id = {group_id}
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
@@ -24,7 +16,6 @@ def index(request):
     template = 'posts/index.html'
     title = 'Последние обновления на сайте'
     posts = Post.objects.order_by('-pub_date')[:10]
-    # Словарь с данными принято называть context
     context = {
         'posts': posts,
         'title': title,
